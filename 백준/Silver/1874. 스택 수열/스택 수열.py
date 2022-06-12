48e0f776e77786f5a0c3cbd923d80ec181cc0002
kjh95044 <48514956+kjh95044@users.noreply.github.com>
2022-06-12 23:44:52 +0900

    [Silver III] Title: 스택 수열, Time: 4148 ms, Memory: 32816 KB -BaekjoonHub

---
+++ 백준/Silver/1874. 스택 수열/스택 수열.py
@@ -0,0 +1,18 @@
+n = int(input())
+stack = list()
+result = list()
+count = 1
+
+for i in range(n):
+  num = int(input())
+  while count <= num:
+    stack.append(count)
+    count += 1
+    result.append("+")
+  if stack[-1] == num:
+    stack.pop()
+    result.append("-")
+  else:
+    print("NO")
+    exit(0)
+print("\n".join(result))
