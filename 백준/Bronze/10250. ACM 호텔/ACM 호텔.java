916cdbabbbf02dafe926abbf403602f7fcb587ce
JiHongKim <48514956+kjh95044@users.noreply.github.com>
2022-07-17 06:39:28 -0700

    [Bronze III] Title: ACM 호텔, Time: 128 ms, Memory: 14264 KB -BaekjoonHub

---
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.io.*;
+ 
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+ 
+		StringBuilder sb = new StringBuilder();
+		int T = Integer.parseInt(br.readLine());
+ 
+		for (int i = 0; i < T; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+ 
+			int H = Integer.parseInt(st.nextToken());
+			int W = Integer.parseInt(st.nextToken());
+			int N = Integer.parseInt(st.nextToken());
+ 
+			if (N % H == 0) {
+				sb.append((H * 100) + (N / H)).append('\n');
+			} else {
+				sb.append(((N % H) * 100) + ((N / H) + 1)).append('\n');
+			}
+		}
+		System.out.print(sb);
+ 
+	}
+}
