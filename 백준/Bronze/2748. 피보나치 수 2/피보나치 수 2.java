8d13bb91f83e6780e974fa1d9ba58ce3e6468fdd
JiHongKim <48514956+kjh95044@users.noreply.github.com>
2022-08-01 21:39:38 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 128 ms, Memory: 14264 KB -BaekjoonHub

---
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	static long[] arr;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		arr = new long[n + 1];
+ 
+		for(int i = 0; i < n + 1; i++) {
+			arr[i] = -1;
+		}
+        
+		arr[0] = 0;
+		arr[1] = 1;
+		System.out.println(Fib(n));
+	}
+	
+	public static long Fib(int n) {
+		if(arr[n] == -1) {
+			arr[n] = Fib(n - 1) + Fib(n - 2);
+		}
+		return arr[n];
+	}
+ 
+}
