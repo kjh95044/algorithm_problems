950bb11de86d1e7ec0c62076e961d85ef6d6925c
JiHongKim <48514956+kjh95044@users.noreply.github.com>
2023-02-19 23:46:17 +0900

    [Gold V] Title: 동전, Time: 124 ms, Memory: 14280 KB -BaekjoonHub

---
+++ 백준/Gold/9084. 동전/동전.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	static StringBuilder sb = new StringBuilder();
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	
+		int T = Integer.parseInt(br.readLine());
+		
+		for (int test_case = 0; test_case < T; test_case++) {
+			int N = Integer.parseInt(br.readLine());
+			int[] arr = new int[N];
+			
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			
+			for(int i=0; i< N; i++) {
+				arr[i] = Integer.parseInt(st.nextToken());
+			}
+			int M = Integer.parseInt(br.readLine());
+			
+			int[] dp = new int[M+1];
+			dp[0] = 1;
+			for(int coin : arr) {
+				for(int j = coin; j<=M; j++) {
+					dp[j] += dp[j - coin];
+				}
+			}
+			
+			sb.append(dp[M]).append("\n");
+		}
+		System.out.println(sb);
+	}
+	
+}
