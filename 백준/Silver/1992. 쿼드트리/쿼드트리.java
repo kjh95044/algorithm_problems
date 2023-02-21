73764b9ab581ec48f7dd275ac328807fe54d67d7
JiHongKim <48514956+kjh95044@users.noreply.github.com>
2023-02-21 17:53:03 +0900

    [Silver I] Title: 쿼드트리, Time: 132 ms, Memory: 15136 KB -BaekjoonHub

--- 백준/Silver/1992. 쿼드트리/쿼드트리.java
+++ 백준/Silver/1992. 쿼드트리/쿼드트리.java
@@ -1,141 +1,51 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
 
 public class Main {
 
 	static StringBuilder sb = new StringBuilder();
-
-	public static void main(String[] args) throws IOException {
+	static int N, map[][];
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-		int N = Integer.parseInt(br.readLine());
-		int[][] map = new int[N][N];
-
+		
+		N = Integer.parseInt(br.readLine());
+		map = new int[N][N];
 		for (int i = 0; i < N; i++) {
-			String[] split = br.readLine().split("");
+			String input = br.readLine();
 			for (int j = 0; j < N; j++) {
-				map[i][j] = Integer.parseInt(split[j]);
+				map[i][j] = (int)(input.charAt(j)-'0');
 			}
 		}
-
-	
-		zip(map);
+		
+		cut(0,0,N);
 		System.out.println(sb);
 	}
 
-	private static void zip(int[][] map) {
-
+	private static void cut(int r, int c, int size) {
+		int first = map[r][c];
+		boolean flag = false;
 		
-
-		/*
-		 * 2 11 11 입력시 1이 나와야하는데 (1111)이 나왔다.
-		 */
-		boolean canPrint = true;
-		int start = map[0][0];
-		for(int i=0; i<map.length; i++){
-			for(int j=0; j<map[0].length; j++) {
-				if(map[i][j]!=start) {
-					canPrint = false;
-					break;
+		for (int i = r; i < r+size; i++) {
+			for (int j = c; j < c+size; j++) {
+				if(first != map[i][j]) {
+					flag = true;
 				}
 			}
 		}
-		if(canPrint) {
-			sb.append(start);
+		
+		if(!flag) {
+			sb.append(first);
 			return;
 		}
-
-		int mid = map.length / 2;
-		int[][] newMap = new int[mid][mid];
 		
-		int x, y;
-
+		int half = size/2;
 		sb.append("(");
-		// 왼쪽 위
-		canPrint = true;
-		x = 0;
-		for (int i = 0; i < mid; i++) {
-			y = 0;
-			for (int j = 0; j < mid; j++) {
-				newMap[x][y++] = map[i][j];
-				if (newMap[0][0] != map[i][j]) {
-					canPrint = false;
-				}
-			}
-			x++;
-		}
-
-		if (canPrint) {
-			sb.append(newMap[0][0]);
-		} else {
-			zip(newMap);
-		}
-
-		// 오른쪽 위
-		canPrint = true;
-		x = 0;
-		for (int i = 0; i < mid; i++) {
-			y = 0;
-			for (int j = mid; j < map[0].length; j++) {
-				newMap[x][y++] = map[i][j];
-				if (newMap[0][0] != map[i][j]) {
-					canPrint = false;
-				}
-			}
-			x++;
-		}
-
-		if (canPrint) {
-			sb.append(newMap[0][0]);
-		} else {
-			zip(newMap);
-		}
-
-		// 왼쪽 아래
-		canPrint = true;
-		x = 0;
-		for (int i = mid; i < map.length; i++) {
-			y = 0;
-			for (int j = 0; j < mid; j++) {
-				newMap[x][y++] = map[i][j];
-				if (newMap[0][0] != map[i][j]) {
-					canPrint = false;
-				}
-			}
-			x++;
-		}
-
-		if (canPrint) {
-			sb.append(newMap[0][0]);
-		} else {
-			zip(newMap);
-		}
-
-		// 오른쪽 아래
-		canPrint = true;
-		x = 0;
-		for (int i = mid; i < map.length; i++) {
-			y = 0;
-			for (int j = mid; j < map.length; j++) {
-				newMap[x][y++] = map[i][j];
-				if (newMap[0][0] != map[i][j]) {
-					canPrint = false;
-				}
-			}
-			x++;
-		}
-
-		if (canPrint) {
-			sb.append(newMap[0][0]);
-		} else {
-
-			zip(newMap);
-
-		}
+		cut(r, c, half);
+		cut(r, c + half, half);
+		cut(r+half, c, half);
+		cut(r+half, c+half, half);
 		sb.append(")");
-		
 	}
 }
