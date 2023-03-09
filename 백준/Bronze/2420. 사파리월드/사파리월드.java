512d5901a6c1f9d966b99d58392d28701955d0ac
JiHongKim <48514956+kjh95044@users.noreply.github.com>
2023-03-09 16:45:08 +0900

    [Bronze V] Title: 사파리월드, Time: 108 ms, Memory: 12852 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+     public static void main(String []args){
+         Scanner sc = new Scanner(System.in);
+         long x = sc.nextLong();
+         long y = sc.nextLong();
+        System.out.println(Math.abs(x-y));
+     }
+}
