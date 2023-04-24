27c0352a35472547c12ca11fd52d860066861934
JiHongKim <48514956+kjh95044@users.noreply.github.com>
2023-04-25 00:24:26 +0900

    [Gold IV] Title: 신기한 키보드, Time: 128 ms, Memory: 15452 KB -BaekjoonHub

--- 백준/Gold/1796. 신기한 키보드/신기한 키보드.java
+++ 백준/Gold/1796. 신기한 키보드/신기한 키보드.java
@@ -14,6 +14,7 @@ public class Main {
 
         char[] inputArray = input.toCharArray();
 
+        // 문자열 S의 각 index 값을 char 별로 hash 에 저장한다.
         for(int i=0; i<inputArray.length; i++) {
             List<Integer> temp = map.get(inputArray[i]);
             if (temp == null) {
@@ -23,9 +24,10 @@ public class Main {
             map.put(inputArray[i], temp);
         }
 
-        
+        // 중복되지 않게 저장하고, 순서대로 정렬한다..
         list = new ArrayList<>(map.keySet());
         Collections.sort(list);
+        
         dfs(0, 0, 0);
 
         // 엔터를 누르는 갯수는 출력할 문자의 갯수와 같다.
@@ -34,6 +36,10 @@ public class Main {
         System.out.println(result);
     }
 
+    // 처음에는 가장 가까운 곳 먼저 들르면 될꺼라 생각했는데,
+    // 반례를 보니 가장 가까운 곳이 최적해가 아니었다.
+    // 그래서 왼쪽을 먼저 가거나 오른쪽을 먼저 가는 경우 둘다 신경 써줘야했고,
+    // 이를 재귀로 풀었다.
     private static void dfs(int index, int cur, int cnt) {
 
         if(cnt > result) return;
@@ -51,11 +57,11 @@ public class Main {
 
 //        System.out.println(min + " " + max);
 
-        if(max == min){
+        if(max == min){ // 문자열 S에 있는 알파벳을 list에 저장 해놨기 때문에 index값을 하나씩 늘려 가면서 끝까지 탐색한다.
             dfs(index+1,max,cnt + Math.abs(cur - min));
         }else {
-            dfs(index+1,max,cnt + Math.abs(cur - min) + Math.abs(max - min));
-            dfs(index+1,min,cnt + Math.abs(cur - max) + Math.abs(max - min));
+            dfs(index+1,max,cnt + Math.abs(cur - min) + Math.abs(max - min)); // 왼쪽 먼저
+            dfs(index+1,min,cnt + Math.abs(cur - max) + Math.abs(max - min)); // 오른쪽 먼저
         }
     }
 }
