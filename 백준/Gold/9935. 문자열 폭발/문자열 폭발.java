2a6728409004a14dda999b06f3c56cbd07a6d403
JiHongKim <48514956+kjh95044@users.noreply.github.com>
2023-04-28 10:29:58 +0900

    [Gold IV] Title: 문자열 폭발, Time: 256 ms, Memory: 26460 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.java
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.java
@@ -37,37 +37,5 @@ public class Main {
 			System.out.println(sb);
 		}
 
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-//		for (int i = 0; i < str.length(); i++) {
-//			char ch = str.charAt(i);
-//			sb.append(ch);
-//			if (sb.length() >= boom.length()) { // sb의 길이가 boom 이상이 되면 확인 boolean
-//				sameFlag = true;
-//				for (int j = 0; j < boom.length(); j++) {
-//					char ch1 = sb.charAt(sb.length() - boom.length() + j);
-//					char ch2 = boom.charAt(j);
-//					if (ch1 != ch2) {
-//						sameFlag = false;
-//						break;
-//					}
-//				}
-//				if (sameFlag) { // 폭발 문자열 삭제
-//					sb.delete(sb.length() - boom.length(), sb.length());
-//				}
-//			}
-//		}
-
-
 	}
 }
