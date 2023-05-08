6676805f7ca52b27b03302a2b8dfcdbd811bbc98
JiHongKim <48514956+kjh95044@users.noreply.github.com>
2023-05-09 00:01:17 +0900

    Update 부등호.java

--- 백준/Silver/2529. 부등호/부등호.java
+++ 백준/Silver/2529. 부등호/부등호.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-public class Main {
+public class 부등호 {
     static char[] arr;
     static int N;
 
@@ -20,10 +20,10 @@ public class Main {
 
         visited = new boolean[10];
         permutation(0, "");
-        Collections.sort(resultList);
-        
-        System.out.println(resultList.get(resultList.size()-1));
-        System.out.println(resultList.get(0));
+        Collections.sort(resultList); // 리스트 정렬
+
+        System.out.println(resultList.get(resultList.size()-1));  // 정렬했으니 마지막 값이 가장 큰 값
+        System.out.println(resultList.get(0)); // 정렬했으니 첫번째 값이 가장 작은 값
 
     }
 
@@ -40,25 +40,25 @@ public class Main {
         for (int i = 0; i < 10; i++) {
             if (visited[i]) continue;
 
-            if (cnt == 0) {
+            if (cnt == 0) { // 처음은 비교 대상이 없어서 무조건 넣기
                 visited[i] = true;
                 permutation(cnt + 1, nums + i);
                 visited[i] = false;
             } else {
                 char now = arr[cnt - 1]; // cnt가 부등호 만족하는 숫자라서 부등호는 숫자 다음
                 boolean isPass = false;
-                int left = nums.charAt(cnt - 1) - '0';
+                int left = nums.charAt(cnt - 1) - '0'; // nums에 있는 마지막 숫자
 
-                if (now == '>') {
+                if (now == '>') { // 부등호가 > 이고 왼쪽 숫자가 오른쪽 숫자보다 크면 pass
                     if (left > i) {
                         isPass = true;
                     }
-                } else {
+                } else { // 부등호가 < 이고 왼쪽 숫자가 오른쪽 숫자보다 작으면 pass
                     if (left < i) {
                         isPass = true;
                     }
                 }
-                if (isPass) {
+                if (isPass) { // 조건에 만족하면 cnt를 증가 시키고 nums에 현재 숫자 추가해준다.
                     visited[i] = true;
                     permutation(cnt + 1, nums + i);
                     visited[i] = false;
