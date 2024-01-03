c699d447acbc7e18ce09f984879bb96089366639
JiHongKim <48514956+kjh95044@users.noreply.github.com>
2024-01-03 17:14:23 +0900

    [Silver I] Title: 기타리스트, Time: 80 ms, Memory: 11780 KB -BaekjoonHub

--- 백준/Silver/1495. 기타리스트/기타리스트.java
+++ 백준/Silver/1495. 기타리스트/기타리스트.java
@@ -1,69 +1,58 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int N;
-    static int S;
-    static int M;
-    static int answer;
-    static int[] arr;
-    static int[] dp;
+    static int N, S, M;
+    static int[] arr, dp;
 
     public static void main(String[] args) throws IOException {
         // 입력
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        answer = -1;
 
         N = Integer.parseInt(st.nextToken());
         S = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         arr = new int[N];
-        dp = new int [M+1];
+        dp = new int[M + 1];
 
-        for(int i=0; i<M+1; i++){
+        // 초기화
+        for (int i = 0; i <= M; i++) {
             dp[i] = -1;
         }
 
-
+        // 배열 입력
         st = new StringTokenizer(br.readLine());
-
-        for(int i =0; i< N; i++){
+        for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
         // 풀이
-        dp[S] = 0; // index가 값, value가 순서
+        dp[S] = 0;
 
-        for(int i=1; i<=N; i++){
-            int value = arr[i-1];
-            List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < N; i++) {
+            int value = arr[i];
+            int[] nextDP = dp.clone(); // 현재 상태를 유지한 채로 다음 dp 배열을 생성
 
-            for(int j=0; j<=M; j++){
-                if(dp[j] == i-1){
-                    if((j + value) <=M){
-                        list.add(j+value);
-                    }
-                    if((j-value)>=0){
-                        list.add(j-value);
-                    }
+            for (int j = 0; j <= M; j++) {
+                if (dp[j] == i) {
+                    int plus = j + value;
+                    int minus = j - value;
+
+                    if (plus <= M) nextDP[plus] = i + 1;
+                    if (minus >= 0) nextDP[minus] = i + 1;
                 }
             }
 
-            for(int v : list){
-                dp[v] = i;
-            }
-
+            dp = nextDP; // 다음 dp 배열로 업데이트
         }
 
+        // 최대값 찾기
         int max = -1;
-        for(int i =0; i<=M; i++){
-            if(dp[i] == N){
+        for (int i = 0; i <= M; i++) {
+            if (dp[i] == N) {
                 max = Math.max(max, i);
             }
         }
