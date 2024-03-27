c7aaa617577005ab883b8eab2645604f44f276ec
JiHongKim <48514956+kjh95044@users.noreply.github.com>
2024-03-27 16:43:54 +0900

    [Bronze II] Title: Hashing, Time: 80 ms, Memory: 11496 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -9,15 +9,23 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         br.readLine();
         String input = br.readLine();
-        int result = 0;
+        long result = 0;
         char[] chars = input.toCharArray();
         int cnt = 0;
         for(char ch : chars){
             int num = ch - 'a' + 1;
-            result += num * Math.pow(31, cnt++) % 1234567891;
+            result += num * myPow(31, cnt++);
         }
 
-        System.out.println(result);
+        System.out.println(result % 1234567891);
 
+    }
+
+    public static long myPow(int a, int b){
+        long result = 1;
+        for(int i=0; i<b; i++){
+            result = (result * a) % 1234567891;
+        }
+        return result;
     }
 }
