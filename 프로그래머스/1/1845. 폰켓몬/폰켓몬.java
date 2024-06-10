f6188ddab716a8dbca7003bf57e4994dcf2d720b
JiHongKim <48514956+kjh95044@users.noreply.github.com>
2024-06-10 11:01:54 +0900

    [level 1] Title: 폰켓몬, Time: 6.21 ms, Memory: 87.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    // 가장 많은 종류의 폰켓몬을 얻으려 한다.
+    // 선택은 N/2 마리 할수있다.
+    
+    public int solution(int[] nums) {
+        int answer = 0;
+        
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i : nums){
+            map.put(i, map.getOrDefault(i,0) + 1);
+        }
+        
+        if(map.size() > nums.length/2){
+            answer = nums.length/2;
+        }else{
+            answer = map.size();
+        }
+        
+        return answer;
+    }
+}
