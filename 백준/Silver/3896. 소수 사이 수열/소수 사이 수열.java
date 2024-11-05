f25c54f06abf1676b9b254bbf8dde3c5316d2c4b
JiHongKim <48514956+kjh95044@users.noreply.github.com>
2024-11-06 01:42:23 +0900

    [Silver I] Title: 소수 사이 수열, Time: 204 ms, Memory: 18484 KB -BaekjoonHub

---
+++ 백준/Silver/3896. 소수 사이 수열/소수 사이 수열.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.io.*;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        boolean prime[] = new boolean[1299710];
+        for(int i=2; i<1299710; i++)
+            for(int j=i; i*j>2 && i*j<1299710; j++)
+                prime[i*j] = true;
+        
+        int t = sc.nextInt();
+        while(t-->0) {
+            int k = sc.nextInt();
+            if(prime[k]) {
+                int s = k;
+                int e = k;
+                while(true) {
+                    if(!prime[--s]) break;
+                }
+                while(true) {
+                    if(!prime[++e]) break;
+                }
+                System.out.println(e-s);
+            } else System.out.println(0);
+        }
+    }
+}
