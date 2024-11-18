35bcc82efb7ab0f195ecc933422760e28e83b070
JiHongKim <48514956+kjh95044@users.noreply.github.com>
2024-11-18 15:52:50 +0900

    [Silver III] Title: 배열 복원하기, Time: 1544 ms, Memory: 263968 KB -BaekjoonHub

---
+++ 백준/Silver/16967. 배열 복원하기/배열 복원하기.java
@@ -0,0 +1,49 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int H = sc.nextInt();
+        int W = sc.nextInt();
+        int X = sc.nextInt();
+        int Y = sc.nextInt();
+
+        int[][] arrA = new int[H][W];
+        int[][] arrB = new int[H+X][W+Y];
+
+        for(int i=0; i<H+X; i++){
+            for(int j=0; j<W+Y; j++){
+                arrB[i][j] = sc.nextInt();
+            }
+        }
+
+        // 위쪽 안 겹치는곳
+        for(int i=0; i<X; i++){
+            for(int j=0; j<W; j++){
+                arrA[i][j] = arrB[i][j];
+            }
+        }
+
+        // 왼쪽 안 겹치는 곳
+        for(int i=X; i<H; i++){
+            for(int j=0; j<W; j++){
+                arrA[i][j] = arrB[i][j];
+            }
+        }
+
+        // 겹치는 곳
+        for(int i=X; i<H; i++) {
+			for(int j=Y; j<W; j++) {
+				arrA[i][j] = arrB[i][j] - arrA[i-X][j-Y];
+			}
+		}
+
+        for(int i=0; i<H; i++){
+            for(int j=0; j<W; j++){
+                System.out.print(arrA[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
